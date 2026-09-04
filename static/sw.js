c71a20299ae602078cef521fb62469186482059c
Idrissa2000 <99462270+Idrissa2000@users.noreply.github.com>
2026-09-04 17:20:48 +0000

    Create sw.js

---
+++ static/sw.js
@@ -0,0 +1,11 @@
+const CACHE = "halal-v1";
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(["/"])).then(()=>self.skipWaiting())
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request)).catch(()=>caches.match("/"))
+  );
+});
